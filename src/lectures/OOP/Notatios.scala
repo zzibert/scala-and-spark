c6da847c368880e778bc31712666696b8fdce18a
Zan Zibert <zan_zibert@hotmail.com>
2020-09-08 16:16:48 +0200

    method notations exercises

--- src/lectures/OOP/Notatios.scala
+++ src/lectures/OOP/Notatios.scala
@@ -2,41 +2,48 @@ package lectures.OOP
 
 object Notatios extends App{
 
-  class Person(val name: String, favouriteMovie: String) {
+  class Person(val name: String, favouriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favouriteMovie
 
-    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(title: String): Person = new Person(name + " the " + title, favouriteMovie, age)
 
-    def unary_! : String = s"${this.name} was here"
+    def unary_+ : Person = new Person(name, favouriteMovie, age+1)
 
     def isAlive : Boolean = true
 
-    def apply(): String = s"Hi, my name is $name and i like $favouriteMovie"
+    def apply(times: Int): String = s"${this.name} watched ${this.favouriteMovie} $times times"
+
+    def learns(subject: String) = s"${this.name} learns $subject"
+
+    def learnsScala : String = this learns "Scala"
   }
 
-  val mary = new Person("mary", "inception")
-
-  println(mary.likes("inception"))
-  println(mary likes "inception") // infix notation - operator notation
+  var mary = new Person("mary", "inception")
 
   // "operators" in Scala
   val tom = new Person("Tom", "fight club")
-  println(mary + tom)
 
-  println(1.+(2)) // all operators are methods
 
   // prefix notation
   val x = -1 // is same as 1.unary_-
   val y = 1.unary_-
   // unary_prefix only works with a few operators -, +, ~, !
 
-  println(!tom)
-  println(tom.unary_!)
 
-  // postfix notation
-  println(mary.isAlive)
+  // overload the + operator
+  println((mary + "rockstar")(3))
 
-  println(mary())
+  // 2. add an age to the person class
+  // add a unary + operator => new person with the age + 1
+  mary = +mary
+  println(mary.age)
 
+  // 3. add a learns method => mary learns
+  // add a learnScala method , calls the learns method with "Scaka"
+  // use it in postfix notation
+  println(mary.learnsScala)
+
+  // overload apply method  mary.apply 2 => "mary watched inception 2 times"
+  println(mary(10))
 
 }
