bb69f83657f87b238eaf6dd538b7e046887b743b
Zan Zibert <zan_zibert@hotmail.com>
2020-12-20 17:41:18 +0100

    Higher-Order-Functions and Curries

--- src/lectures/FunctionalProgramming/HOFsAndCurries.scala
+++ src/lectures/FunctionalProgramming/HOFsAndCurries.scala
@@ -2,42 +2,32 @@ package lectures.FunctionalProgramming
 
 object HOFsAndCurries extends App {
 
-
   // function that applies a function n times over a value x
-  val nTimes: (Int, Int, (Int => Int)) => Int = (x, n, func) => {
-    if (n < 1) x
-    else nTimes(func(x), n-1, func)
+  //nTimes(f, n, x)
+  //nTimes(f, 3, x) => f(f(f(x)))
+
+  def nTimes(f: (Int => Int), n: Int, x: Int): Int = {
+    if (n <= 1) f(x)
+    else nTimes(f, n-1, f(x))
   }
 
-  val nTimesSecond: (Int, (Int => Int)) => (Int => Int) = (n, func) => {
-    if (n <= 0) (x: Int) => x
-    else (x: Int) => nTimesSecond(n-1, func)(func(x))
+  println(nTimes(_ * 3, 3, 1))
+
+  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = { (x: Int) =>
+    if (n <= 1) f(x)
+    else nTimesBetter(f, n - 1)(f(x))
   }
 
-  val multiplyByTwo: (Int => Int) = x => x * 2
+  val multiplyByTwoThreeTimes = nTimesBetter(_ * 2, 3)
 
-  val addTwo: (Int => Int) = x => x + 2
-
-  val plusOne: Int => Int = _ + 1
-
-  val multiplyThreeTimes = nTimesSecond(3, x => x * 2)
-  val divideByTwoFiveTimes = nTimesSecond(5, x => x / 2)
-
-  println(multiplyThreeTimes(2))
-  println(divideByTwoFiveTimes(128))
-
-  val increment10 = nTimesSecond(10, plusOne)
-
-  println(increment10(1))
+  println(multiplyByTwoThreeTimes(1))
+  println(multiplyByTwoThreeTimes(2))
+  println(multiplyByTwoThreeTimes(3))
 
   // curried functions
-  val superAdder: Int => Int => Int = (x: Int) => (y: Int) => x + y
-
-  println(superAdder(5)(4))
-
-  val addFive = superAdder(5)
-
-  println(addFive(10))
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(3)
+  println(add3(10))
 
   // functions with multiple parameter lists
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
@@ -45,13 +35,7 @@ object HOFsAndCurries extends App {
   val standardFormat: (Double => String) = curriedFormatter("%4.2f")
   val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
 
-  println(standardFormat(math.Pi))
-  println(preciseFormat(math.Pi))
-
-  // 2. toCurry(f: (Int, Int) => Int) => (Int => Int => Int)
-//  val toCurry:(f: (Int, Int) => Int)) =  (Int => Int => Int) = {
-//
-//  }
-
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
 
 }
