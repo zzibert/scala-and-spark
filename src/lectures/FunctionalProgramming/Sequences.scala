778918aade703871d904ac5555e8579b49207216
Zan Zibert <zan_zibert@hotmail.com>
2020-12-21 17:37:24 +0100

    Sequences: List, Array, Vector

---
+++ src/lectures/FunctionalProgramming/Sequences.scala
@@ -0,0 +1,64 @@
+package lectures.FunctionalProgramming
+
+import scala.util.Random
+
+object Sequences extends App {
+
+  // Seq
+  val aSequence = Seq(1, 2, 3, 4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(5, 6, 7))
+  println(Seq(7, 6, 5).sorted)
+
+  // Ranges
+  val aRange: Seq[Int] = 1 to 10
+  aRange.foreach(println)
+
+  (1 to 10).foreach(x => println("Hello"))
+
+  // Lists
+  val aList = List(1, 2, 3)
+  val prepended = 42 +: aList :+ 89
+  println(prepended)
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+
+  // Array
+  val numbers = Array(1, 2, 3, 4)
+  val threeElements = Array.ofDim[String](3)
+  threeElements.foreach(println)
+  // mutation
+  numbers(2) = 0 // syntax sugar for numbers.update(2, 0)
+  println(numbers.mkString(" "))
+
+  // Arrays and Sequences
+  val numbersSeq: Seq[Int] = numbers
+  println(numbersSeq)
+
+  val vector: Vector[Int] = Vector(1, 2, 3)
+  println(vector)
+
+  // vectors vs lists
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt())
+      System.nanoTime() - currentTime
+    }
+    times.sum * 1.0 / maxRuns // average time to the collection by updated
+  }
+
+  val numbersList = (1 to maxCapacity).toList
+  val numbersVector = (1 to maxCapacity).toVector
+
+  println(getWriteTime(numbersList))
+  println(getWriteTime(numbersVector))
+}
