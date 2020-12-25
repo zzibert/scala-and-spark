13a6d8fb63c14dab459caeaaa0d8165913ac1946
Zan Zibert <zan_zibert@hotmail.com>
2020-12-25 20:23:41 +0100

    Enhancing A Functional Set

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -11,6 +11,16 @@ trait MySet[A] extends (A => Boolean) {
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
   def apply(elem: A): Boolean = contains(elem) // DONE
+
+  def -(elem: A): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A]
+  def &(anotherSet: MySet[A]): MySet[A]
+
+  def unary_! : MySet[A]
+
+  // 1.  removing an element
+  // intersection with another set
+  // -- with another set
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -22,6 +32,33 @@ class EmptySet[A] extends MySet[A] {
   def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B] // DONE
   def filter(predicate: A => Boolean): MySet[A] = this // DONE
   def foreach(f: A => Unit): Unit = () // DONE
+
+  def -(elem: A): MySet[A] = this
+  def &(anotherSet: MySet[A]): MySet[A] = this
+  def --(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new AllInclusiveSet[A]
+}
+
+class AllInclusiveSet[A] extends MySet[A] {
+  def contains(elem: A): Boolean = true
+  def +(elem: A): MySet[A] = this
+  def ++(anotherSet: MySet[A]): MySet[A] = this
+
+  def map[B](f: A => B): MySet[B] = ???
+  def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+  def filter(predicate: A => Boolean): MySet[A] = ??? // property-based set
+  def foreach(f: A => Unit): Unit = ???
+
+  def -(elem: A): MySet[A] = ???
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def unary_! : MySet[A] = new EmptySet[A]
+}
+
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  
 }
 
 case class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -48,6 +85,17 @@ case class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail foreach f
   }
+
+  def -(elem: A): MySet[A] = { // DONE
+    if (head == elem) tail
+    else tail - elem + head
+  }
+
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  def unary_! : MySet[A] = (x: A) => !this.contains(x)
 }
 
 object MySet {
