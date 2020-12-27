3aa7818ef3fa713eebe389914b9b287b40452081
Zan Zibert <zan_zibert@hotmail.com>
2020-12-27 20:01:23 +0100

    Infinite Streams Proficiency: More Exercises

--- src/exercises/StreamsPlayground.scala
+++ src/exercises/StreamsPlayground.scala
@@ -1,7 +1,30 @@
 package exercises
 
 object StreamsPlayground extends App {
+
+
+
+
+  // Exercises on streams
+  // 1  - stream of fibonnaci numbers
+  // 2 - stream of prime numbers with Eratosthenes's sieve
+
   val naturals = MyStream.from(1)(_ + 1)
+  println(naturals.take(10).toList())
+
+  var fibMap: Map[Int, Int] = Map((1 -> 2))
+
+  def fibonacci(first: Int, second: Int): MyStream[Int] = { // DONE
+    new ConsStream(first, fibonacci(second, first + second))
+  }
+
+  def eratosthenes(numbers: MyStream[Int]): MyStream[Int] = {
+    if (numbers.isEmpty) numbers
+    else new Cons(numbers.head, eratosthenes(numbers.tail.filter(_ % numbers.head != 0)))
+  }
+
+  println(fibonacci(1, 1).take(100).toList())
+  // 1, 1, 2, 3, 5, 8, 13, 21, 35
 }
 
   abstract class MyStream[+A] {
@@ -10,7 +33,7 @@ object StreamsPlayground extends App {
     def tail: MyStream[A]
 
     def #::[B >: A](element: B): MyStream[B] // prepend operator
-    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // concatenate two streams
+    def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] // concatenate two streams
 
     def foreach(f: A => Unit): Unit
     def map[B](f: A => B): MyStream[B]
@@ -30,8 +53,8 @@ object StreamsPlayground extends App {
     def head: Nothing = throw new NoSuchElementException
     def tail: Nothing = throw new NoSuchElementException
 
-    def #::[B >: Nothing](element: B): MyStream[B] = new Cons[B](element, this)
-    def ++[B >: Nothing](anotherStream: MyStream[B]): MyStream[B] = anotherStream
+    def #::[B >: Nothing](element: B): MyStream[B] = new ConsStream[B](element, this)
+    def ++[B >: Nothing](anotherStream: => MyStream[B]): MyStream[B] = anotherStream
 
     def foreach(f: Nothing => Unit): Unit = ()
     def map[B](f: Nothing => B): MyStream[B] = this
@@ -42,33 +65,33 @@ object StreamsPlayground extends App {
     def takeAsList(n: Int): List[Nothing] = Nil
   }
 
-  class Cons[+A](h: A, t: => MyStream[A]) extends MyStream[A] {
+  class ConsStream[A](h: A, t: => MyStream[A]) extends MyStream[A] {
     def isEmpty: Boolean = false
     override val head: A = h
 
     override lazy val tail: MyStream[A] = t // call by need
 
-    def #::[B >: A](element: B): MyStream[B] = new Cons[B](element, this)
-    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
+    def #::[B >: A](element: B): MyStream[B] = new ConsStream[B](element, this)
+    def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] = new ConsStream(head, tail ++ anotherStream)
 
     def foreach(f: A => Unit): Unit = {
       f(head)
       tail foreach f
     }
 
-    def map[B](f: A => B): MyStream[B] = new Cons[B](f(h), tail.map(f)) // preserves lazy evaluation
+    def map[B](f: A => B): MyStream[B] = new ConsStream[B](f(h), tail.map(f)) // preserves lazy evaluation
 
     def flatMap[B](f: A => MyStream[B]): MyStream[B] = f(head) ++ tail.flatMap(f)
 
     def filter(predicate: A => Boolean): MyStream[A] = {
-      if(predicate(head)) new Cons(head, tail.filter(predicate))
+      if(predicate(head)) new ConsStream(head, tail.filter(predicate))
       else tail.filter(predicate)
     }
 
     def take(n: Int): MyStream[A] = {
       if (n <= 0) EmptyStream
-      else if (n == 1) new Cons(head, EmptyStream)
-      else new Cons(head, tail.take(n-1))
+      else if (n == 1) new ConsStream(head, EmptyStream)
+      else new ConsStream(head, tail.take(n-1))
     }
 
     def takeAsList(n: Int): List[A] = {
@@ -78,6 +101,6 @@ object StreamsPlayground extends App {
   }
 
   object MyStream {
-    def from[A](start: A)(generator: A => A): MyStream[A] = new Cons(start, MyStream.from(generator(start))(generator))
+    def from[A](start: A)(generator: A => A): MyStream[A] = new ConsStream(start, MyStream.from(generator(start))(generator))
   }
 
