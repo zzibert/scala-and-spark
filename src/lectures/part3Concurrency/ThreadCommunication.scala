3be31fa0f3fc59d566f86d7f7fcb6351a553f808
Zan Zibert <zan_zibert@hotmail.com>
2020-12-28 18:38:51 +0100

    Producer-Consumer, Level 3 + Excercises

--- src/lectures/part3Concurrency/ThreadCommunication.scala
+++ src/lectures/part3Concurrency/ThreadCommunication.scala
@@ -11,12 +11,13 @@ object ThreadCommunication extends App {
   * */
 
   class SimpleContainer {
-    private var queue = ArrayBuffer[Int]()
+    private var queue = ArrayBuffer[Int](1)
 
     def isEmpty: Boolean = queue.size == 0
     def set(newValue: Int): Unit = queue += newValue
     def get: Int = {
       val result = queue.remove(0)
+      set(result + 1)
       result
     }
   }
@@ -47,57 +48,107 @@ object ThreadCommunication extends App {
 //  naiveProdCons()
 
   // wait and notify
-  def prodConsLargeBuffer(): Unit = {
     val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
-    val capacity = 3
+    val capacity = 500
 
-    val consumer = new Thread(() => {
+    class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+      override def run(): Unit = {
+        val random = new Random()
+        while(true) {
+          buffer.synchronized {
+            while (buffer.isEmpty) {
+              println(s"[consumer $id] buffer empty, waiting...")
+              buffer.wait()
+            }
+            val value = buffer.dequeue
+            println(s"[consumer $id] im consuming " + value)
+            buffer.notify()
+
+          }
+          Thread.sleep(random.nextInt(500))
+        }
+      }
+    }
+
+    def consumer(index: Int, buffer: mutable.Queue[Int]): Thread = {
+      new Thread(() => {
+        val random = new Random()
+        while(true) {
+          buffer.synchronized {
+            if (buffer.isEmpty) {
+              println(s"[consumer $index] buffer empty, waiting...")
+              buffer.wait()
+            } else {
+              val value = buffer.dequeue
+              println(s"[consumer $index] im consuming " + value)
+              buffer.notify()
+            }
+          }
+          Thread.sleep(random.nextInt(2000))
+        }
+      })
+    }
+
+  @volatile var generator = 1
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
       val random = new Random()
-
       while(true) {
         buffer.synchronized {
-          if (buffer.isEmpty) {
-            println("[consumer] buffer empty, waiting...")
+          while (buffer.size == capacity) {
+            println(s"[producer $id] buffer is currently full, waiting...")
             buffer.wait()
-          } else {
-            val value = buffer.dequeue()
-            println("[consumer] im consuming " + value)
-            buffer.notify()
           }
+          generator += 1
+          val value = generator
+          println(s"[producer $id] producing value: " + value)
+          buffer.addOne(value)
+          buffer.notify()
+
         }
-        Thread.sleep(random.nextInt(5000))
-
+        Thread.sleep(random.nextInt(250))
       }
-
-    })
-    val producer = new Thread(() => {
-      var value = 1
-      val random = new Random(
-
-      )
-      while(true) {
-        buffer.synchronized {
-          if (buffer.size == capacity) {
-            println("[producer] buffer is currently full, waiting...")
-            buffer.wait()
-          } else {
-            value += 1
-            println("[producer] producing value: " + value)
-            buffer.addOne(value)
-            buffer.notify()
-          }
-        }
-        Thread.sleep(random.nextInt(500))
-      }
-
-    })
-
-    producer.start()
-    consumer.start()
+    }
   }
 
-  prodConsLargeBuffer()
+    def producer(index: Int, buffer: mutable.Queue[Int]): Thread = {
+      new Thread(() => {
+        val random = new Random()
+        while(true) {
+          buffer.synchronized {
+            if (buffer.size == capacity) {
+              println(s"[producer $index] buffer is currently full, waiting...")
+              buffer.wait()
+            } else {
+              generator += 1
+              val value = generator
+              println(s"[producer $index] producing value: " + value)
+              buffer.addOne(value)
+              buffer.notify()
+            }
+          }
+          Thread.sleep(random.nextInt(250))
+        }
+      })
+    }
+
+  val consumers = (1 to 5).map(new Consumer(_, buffer)).toList
+
+  val producers = (1 to 5).map(new Producer(_, buffer, capacity)).toList
+
+  producers.foreach(_.start())
+  consumers.foreach(_.start())
+
+//  prodConsLargeBuffer()
   /*
   * producer -> [? ? ? ] -> consumer
   * */
+
+  /*
+  * producer1 -> [? ? ?] -> consumer1
+  * producer2 -> same buffer -> consumer2
+  * */
+
+
 }
